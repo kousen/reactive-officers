cb917dd73134d38dc349a0060b508596ae34f97a
Ken Kousen <ken.kousen@kousenit.com>
2018-02-27 07:52:58 -0500

    Remove unused imports

--- src/main/java/com/oreilly/OfficerInit.java
+++ src/main/java/com/oreilly/OfficerInit.java
@@ -5,18 +5,8 @@ import com.oreilly.entities.Officer;
 import com.oreilly.entities.Rank;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.data.mongodb.core.CollectionOptions;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.ReactiveMongoOperations;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Component
 public class OfficerInit implements ApplicationRunner {
