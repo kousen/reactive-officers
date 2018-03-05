1be54036249ab7ad56a0b5d6e5f30421dd1b7cff
Ken Kousen <ken.kousen@kousenit.com>
2018-03-04 19:19:13 -0600

    Upgrade to JUnit 5

--- src/test/java/com/oreilly/controllers/ReactiveOfficersApplicationTests.java
+++ src/test/java/com/oreilly/controllers/ReactiveOfficersApplicationTests.java
@@ -4,18 +4,18 @@ import com.oreilly.dao.OfficerRepository;
 import com.oreilly.entities.Officer;
 import com.oreilly.entities.Rank;
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
 import java.util.Collections;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ReactiveOfficersApplicationTests {
 
--- src/test/java/com/oreilly/dao/OfficerRepositoryTest.java
+++ src/test/java/com/oreilly/dao/OfficerRepositoryTest.java
@@ -2,12 +2,12 @@ package com.oreilly.dao;
 
 import com.oreilly.entities.Officer;
 import com.oreilly.entities.Rank;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -15,7 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @SuppressWarnings("Duplicates")
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class OfficerRepositoryTest {
     @Autowired
@@ -28,7 +28,7 @@ public class OfficerRepositoryTest {
             new Officer(Rank.CAPTAIN, "Kathryn", "Janeway"),
             new Officer(Rank.CAPTAIN, "Jonathan", "Archer"));
 
-    @Before
+    @BeforeEach
     public void setUp() {
         repository.deleteAll()
                   .thenMany(Flux.fromIterable(officers))
